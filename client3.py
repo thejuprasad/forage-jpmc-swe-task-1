a350957ca190ec60b8c22bba563f1d8fe8416504
Theju Prasad <tthejuprasad00@gmail.com>
2023-10-05 15:43:59 +0530

    normal

--- client3.py
+++ client3.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        """ ----------- Update to get ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock]=price
